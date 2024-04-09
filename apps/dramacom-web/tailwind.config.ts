4bfc660c97c60458d14c73e876b4c687ccf44015
ryone9re <ryoichi.kurimoto@gmail.com>
2024-04-09 18:10:37 +0000

    revive pages functions

--- apps/dramacom-web/tailwind.config.ts
+++ apps/dramacom-web/tailwind.config.ts
@@ -2,9 +2,6 @@ import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./app/**/*.{ts,tsx}"],
   plugins: [daisyui],
 } satisfies Config;
