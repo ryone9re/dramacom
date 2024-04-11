23f956eebdbb0445c3fb48d82534ac519e2d6568
You-keitou <youkeitou327@gmail.com>
2024-04-11 16:36:03 +0000

    typeエラー修正

--- apps/dramacom-web/app/components/atoms/icons.tsx
+++ apps/dramacom-web/app/components/atoms/icons.tsx
@@ -6,6 +6,7 @@ export const KeyIcon = () => {
       fill="currentColor"
       className="w-4 h-4 opacity-70"
     >
+      <title>icon</title>
       <path
         fillRule="evenodd"
         d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
@@ -23,6 +24,7 @@ export const EmailIcon = () => {
       fill="currentColor"
       className="w-4 h-4 opacity-70"
     >
+      <title>icon</title>
       <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
       <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
     </svg>
@@ -37,6 +39,7 @@ export const UserIcon = () => {
       fill="currentColor"
       className="w-4 h-4 opacity-70"
     >
+      <title>icon</title>
       <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
     </svg>
   );
@@ -50,6 +53,7 @@ export const SearchIcon = () => {
       fill="currentColor"
       className="w-4 h-4 opacity-70"
     >
+      <title>icon</title>
       <path
         fillRule="evenodd"
         d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
@@ -69,7 +73,8 @@ export const StarIcon = () => {
       className="w-4 h-4 ml-1"
       viewBox="0 0 24 24"
     >
-      <path d="M12 2l2.5 7.5L22 12l-7.5 2.5L12 22l-2.5-7.5L2 12l7.5-2.5L12 2z"></path>
+      <title>icon</title>
+      <path d="M12 2l2.5 7.5L22 12l-7.5 2.5L12 22l-2.5-7.5L2 12l7.5-2.5L12 2z" />
     </svg>
   );
 };
@@ -84,6 +89,7 @@ export const PlayIcon = () => {
       stroke="currentColor"
       className="w-6 h-6"
     >
+      <title>icon</title>
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -103,6 +109,7 @@ export const LikeIcon = () => {
       stroke="currentColor"
       className="w-6 h-6"
     >
+      <title>icon</title>
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
